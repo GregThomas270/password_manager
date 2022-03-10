2af41c0d81c844e5f18adc029fb9a1e4be8211ad
Greg Thomas <gregthesoundguy@yahoo.com>
2022-03-10 15:35:58 -0500

    Why is GitHub so confusing?

--- main.py
+++ main.py
@@ -1,26 +1,84 @@
 from tkinter import *
-
+from tkinter import messagebox
+import json
+from random import choice, randint, shuffle
+import pyperclip
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
 def save():
     website = website_entry.get()
-    type(website)
     email = email_user_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
-    with open("data.txt", mode="a") as file:
-        file.write(f"{website} | {email} | {password}\n")
+    if len(website) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please do not leave any field blank.")
+    else:
+        try:
+            with open("data.json", mode="r") as data_file:
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open("data.json", mode="w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", mode="w") as data_file:
+                json.dump(data, data_file, indent=4)
         website_entry.delete(0, "end")
         password_entry.delete(0, "end")
         website_entry.focus()
 
 
+def search():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showerror(title="Oops", message="No entry has been created.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email:  {email}\npassword:  {password}")
+        else:
+            messagebox.showerror(title="Oops", message="No website by that name found.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+
+
 window = Tk()
+
+
 window.title("Password Manager")
 window.config(padx=50, pady=50)
 
@@ -31,8 +89,8 @@ canvas.grid(column=1, row=0)
 
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1, sticky="e")
-website_entry = Entry(width=45)
-website_entry.grid(column=1, row=1, columnspan=2)
+website_entry = Entry(width=27)
+website_entry.grid(column=1, row=1)
 website_entry.focus()
 
 email_user_label = Label(text="email/username:")
@@ -46,10 +104,13 @@ password_label.grid(column=0, row=3, stick="e")
 password_entry = Entry(width=27)
 password_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=35, command=save)
 add_button.grid(column=1, row=4, columnspan=2)
+
+search_button = Button(text="Search", width=15, command=search)
+search_button.grid(column=2, row=1)
 
 window.mainloop()
